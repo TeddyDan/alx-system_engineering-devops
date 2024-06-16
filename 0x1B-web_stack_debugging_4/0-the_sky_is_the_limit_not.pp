2de2fa8e09bfbb45cafa6b0d2bec47f9ea234824
TeddyDan <muretidanteddy1@gmail.com>
2024-06-16 11:40:33 +0000

    updates

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,6 @@
+# Fixes a faulty wordpress site
 exec { 'fix-wordpress':
-  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ 
 /var/www/html/wp-settings.php; service apache2 restart"',
-  path    => '/usr/bin:/usr/sbin:/bin'
+ i path    => '/usr/bin:/usr/sbin:/bin'
 }
